965728f0224cb6fd3f964b54796d30a37a780a83
jalal-tahae <jalaltahaeri@gmail.com>
2025-12-19 18:22:30 +0330

    HWW6 2.0

--- js.js
+++ js.js
@@ -1,5 +1,7 @@
 function male() {
     console.log(document.getElementsByClassName("form"));
-    var form = document.getElementsByClassName("form")
-    form.classList.add("bgblue");
+
+    var element = document.getElementById("form");
+    element.classList.toggle("bgblue");
 }
+
